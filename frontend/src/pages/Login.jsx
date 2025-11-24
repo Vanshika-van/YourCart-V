ee304629ea409bc62e6d027f92732f247283a601
Vanshika-van <aggarwalvanshika49@gmail.com>
2025-11-25 00:15:59 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -22,7 +22,7 @@ function Login() {
     setError("");
     try {
       const result = await axios.post(
-        `${process.env.REACT_APP_API_URL}/api/auth/login`,
+      `${import.meta.env.VITE_API_URL}/api/auth/login`,
         { email, password },
         { withCredentials: true }
       );
