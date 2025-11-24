4610a7e8c55f2dab45a91b2dce9e15c3f706222f
Vanshika-van <aggarwalvanshika49@gmail.com>
2025-11-24 23:36:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["https://yourcart-v-frontend.onrender.com", "http://localhost:5174"],
   credentials: true,
 }));
 
