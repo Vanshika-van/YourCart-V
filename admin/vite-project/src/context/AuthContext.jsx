720bc7d28726cc603029d8d28da224c2ab3cbb04
Vanshika-van <aggarwalvanshika49@gmail.com>
2025-11-25 11:34:10 +0530

    Update AuthContext.jsx

--- admin/vite-project/src/context/AuthContext.jsx
+++ admin/vite-project/src/context/AuthContext.jsx
@@ -3,6 +3,7 @@ import React, { createContext } from "react";
 export const authDataContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+  // Use environment variable or fallback to localhost
   const serverUrl =
     process.env.REACT_APP_SERVER_URL || "http://localhost:8000";
 
