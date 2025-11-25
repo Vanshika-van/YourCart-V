395f6b0f2acc9779ff9a3bc377897bb749af9577
Vanshika-van <aggarwalvanshika49@gmail.com>
2025-11-25 11:26:17 +0530

    Update AuthContext.jsx

--- admin/vite-project/src/context/AuthContext.jsx
+++ admin/vite-project/src/context/AuthContext.jsx
@@ -1,18 +1,14 @@
-import React, { createContext } from 'react';
+import React, { createContext } from "react";
 
-// Create the context
 export const authDataContext = createContext();
 
-// Context provider component
-function AuthContext({ children }) {
-  // Backend server URL
-  const serverUrl = "https://yourcart-v-backend.onrender.com";
+export const AuthContextProvider = ({ children }) => {
+  const serverUrl =
+    process.env.REACT_APP_SERVER_URL || "http://localhost:8000";
 
   return (
     <authDataContext.Provider value={{ serverUrl }}>
       {children}
     </authDataContext.Provider>
   );
-}
-
-export default AuthContext;
+};
